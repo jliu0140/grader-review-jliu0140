5cfb9ad1d00d0e404de4b4ff0cbdd1a4e9c0d23b
jliu0140 <122562868+jliu0140@users.noreply.github.com>
2023-03-02 10:43:20 -0800

    updated tester

--- TestListExamples.java
+++ TestListExamples.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 class IsMoon implements StringChecker {
   public boolean checkString(String s) {
@@ -17,5 +18,41 @@ public class TestListExamples {
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMerge() {
+    List<String> left = Arrays.asList("a", "d");
+    List<String> right = Arrays.asList("a", "b","c");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+    assertEquals(expected, merged);
+  }
+
+  @Test
+  public void testFilter() {
+      String[] ar = {"moon","wer","asdf","rty"};
+      List<String> list = Arrays.asList(ar);
+      String[] arr = new String[]{"moon"};
+      StringChecker sc = new IsMoon();
+      assertEquals(Arrays.asList(arr),ListExamples.filter(list, sc));
+  }
+
+  @Test
+  public void testFilterEmpty() {
+      List<String> list = new ArrayList<>();
+      String[] arr = {};
+      StringChecker sc = new IsMoon();
+      assertEquals(Arrays.asList(arr), ListExamples.filter(list,sc));
+  }
+
+  @Test(timeout = 500)
+  public void testMergeSimple() {
+      List<String> list1 =new ArrayList<>();
+      list1.add("a");
+      List<String> list2 = new ArrayList<>();
+      list2.add("b");
+      String[] arr = {"a","b"};
+      assertEquals(Arrays.asList(arr),ListExamples.merge(list1,list2));
   }
 }
