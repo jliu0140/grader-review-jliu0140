a3517d9708f46b360911ddf11090ba150fabb8a8
jliu0140 <122562868+jliu0140@users.noreply.github.com>
2023-03-02 10:14:54 -0800

    updated grader

--- grade.sh
+++ grade.sh
@@ -1,5 +1,30 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+cd student-submission
+
+if [[ -e ListExamples.java ]]
+then
+    echo 'File ListExamples.java found'
+else
+    echo 'Missing file ListExamples.java'
+    exit
+fi
+
+cp ../TestListExamples.java ./
+
+javac -cp $CPATH *.java
+if [[ $? -ne 0 ]]
+then
+    echo 'Compiler failed'
+    exit
+else
+    echo 'Compiler passed'
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples #> results.txt
+#cat results.txt
+#grep "failure:" results.txt
+#grep "test" results.txt
